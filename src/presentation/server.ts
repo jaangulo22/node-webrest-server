b502e5c6d52616a2688b366b5e7c1c6edec740a5
Jose Antonio <ja.angulo22@info.uas.edu.mx>
2026-04-20 12:53:15 -0700

    fin seccion 14

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, { Router } from 'express'
 import path from 'node:path';
-import { backup } from 'node:sqlite';
+
 
 interface Options {
     port: number;
+    routes: Router;
     public_path: string;
 }
 
@@ -13,13 +14,15 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
 
 
     constructor(options: Options){
-        const { port, public_path = 'public '} = options;
+        const { port, routes,  public_path = 'public '} = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
     
 
@@ -28,12 +31,20 @@ export class Server {
     async start() {
         
         //* Middlewares
+        this.app.use( express.json() );  // raw
+        this.app.use( express.urlencoded({ extended: true }) );  // x-www-form-urlencoded
+
         
         //* Public Folder
         this.app.use( express.static( this.publicPath ));
 
 
+        //* Routes
+        this.app.use( this.routes );
 
+
+
+        //* SPA
         this.app.get('/{*splat}', (req, res) => {
             const indexPaht = path.join(__dirname + `../../../${ this.publicPath }/index.html`);
             res.sendFile(indexPaht);
